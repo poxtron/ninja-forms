758eb67252fbfd32be79c9fed2ef4ac120c2b2f7
Miguel Sirvent <miguel.sirvent@gmail.com>
2015-01-08 19:47:41 -0600

    Moving the $json variable
    If one passes an error though the ninja_forms_process action then the $json variable will not be set and one gets the error : 
    Undefined variable: json in ..../wp-content/plugins/ninja-forms/includes/display/processing/post-process.php according to the documentation one should be able to add errors on that action.

--- includes/display/processing/post-process.php
+++ includes/display/processing/post-process.php
@@ -4,7 +4,7 @@ function ninja_forms_post_process(){
 
 	$ajax = $ninja_forms_processing->get_form_setting('ajax');
 	$form_id = $ninja_forms_processing->get_form_ID();
-
+	$json = ninja_forms_json_response();
 	if(!$ninja_forms_processing->get_all_errors()){
 
 		do_action('ninja_forms_post_process');
@@ -12,8 +12,6 @@ function ninja_forms_post_process(){
 		if( !$ninja_forms_processing->get_all_errors() ){
 
 			$ninja_forms_processing->update_form_setting( 'processing_complete', 1 );
-
-			$json = ninja_forms_json_response();
 			
 			if($ajax == 1){
 				//header('Content-Type', 'application/json');
